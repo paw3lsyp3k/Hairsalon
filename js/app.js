d0113bd8433ab22f186f56fcec92eec24eefa64d
Paweł <paw3lsyp3k@gmail.com>
2021-02-17 21:33:32 +0100

    landing page upgrades

--- js/app.js
+++ js/app.js
@@ -1,86 +1,17 @@
-function show1() {
-    var x = 0;
-    for (i = 1; i < 7; i++) {
-      x++;
-      document.getElementById("covidanswer" + x).style.display = "none";
-      document.getElementById("covidicon" + x).style.display = "block";
-      document.getElementById("covidicon" + x + x).style.display = "none";
-    }
-    document.getElementById("covidanswer1").style.display = "block";
-    document.getElementById("covidicon1").style.display = "none";
-    document.getElementById("covidicon11").style.display = "block";
-  }
-  
-  function show2() {
-    var x = 0;
-    for (i = 1; i < 7; i++) {
-      x++;
-      document.getElementById("covidanswer" + x).style.display = "none";
-      document.getElementById("covidicon" + x).style.display = "block";
-      document.getElementById("covidicon" + x + x).style.display = "none";
-    }
-    document.getElementById("covidanswer2").style.display = "block";
-    document.getElementById("covidicon2").style.display = "none";
-    document.getElementById("covidicon22").style.display = "block";
-  }
-  
-  function show3() {
-    var x = 0;
-    for (i = 1; i < 7; i++) {
-      x++;
-      document.getElementById("covidanswer" + x).style.display = "none";
-      document.getElementById("covidicon" + x).style.display = "block";
-      document.getElementById("covidicon" + x + x).style.display = "none";
-    }
-    document.getElementById("covidanswer3").style.display = "block";
-    document.getElementById("covidicon3").style.display = "none";
-    document.getElementById("covidicon33").style.display = "block";
-  }
-  function show4() {
-    var x = 0;
-    for (i = 1; i < 7; i++) {
-      x++;
-      document.getElementById("covidanswer" + x).style.display = "none";
-      document.getElementById("covidicon" + x).style.display = "block";
-      document.getElementById("covidicon" + x + x).style.display = "none";
-    }
-    document.getElementById("covidanswer4").style.display = "block";
-    document.getElementById("covidicon4").style.display = "none";
-    document.getElementById("covidicon44").style.display = "block";
-  }
-  
-  function show5() {
-    var x = 0;
-    for (i = 1; i < 7; i++) {
-      x++;
-      document.getElementById("covidanswer" + x).style.display = "none";
-      document.getElementById("covidicon" + x).style.display = "block";
-      document.getElementById("covidicon" + x + x).style.display = "none";
-    }
-    document.getElementById("covidanswer5").style.display = "block";
-    document.getElementById("covidicon5").style.display = "none";
-    document.getElementById("covidicon55").style.display = "block";
-  }
-  
-  function show6() {
-    var x = 0;
-    for (i = 1; i < 7; i++) {
-      x++;
-      document.getElementById("covidanswer" + x).style.display = "none";
-      document.getElementById("covidicon" + x).style.display = "block";
-      document.getElementById("covidicon" + x + x).style.display = "none";
-    }
-    document.getElementById("covidanswer6").style.display = "block";
-    document.getElementById("covidicon6").style.display = "none";
-    document.getElementById("covidicon66").style.display = "block";
-  }
 
-  function show7() {
-    var x = 0;
-    for (i = 1; i < 7; i++) {
+
+  function desc(number){
+    let x=0;
+    for (let i = 1; i < 7; i++) {
       x++;
-      document.getElementById("covidanswer" + x).style.display = "none";
-      document.getElementById("covidicon" + x).style.display = "block";
-      document.getElementById("covidicon" + x + x).style.display = "none";
+      document.getElementById("covidanswer"+ x).style.display = "none";
+      document.getElementById("covidicon"+ x).style.display = "block";
+      document.getElementById("covidicon"+ x + x).style.display = "none";
+    }
+    if(number!=null){
+      document.getElementById("covidanswer"+number).style.display = "block";
+      document.getElementById("covidicon"+number).style.display = "none";
+      document.getElementById("covidicon"+number+number).style.display = "block";
     }
   }
+  
